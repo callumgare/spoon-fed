9858ec0e168e465a8225697334f4041b1410d6bf
Callum Gare <callum@gare.au>
2025-05-21 17:02:07 +1000

    Add message about viewing selected recipes in paprika

--- components/SelectedRecipesInfoBar.vue
+++ components/SelectedRecipesInfoBar.vue
@@ -17,7 +17,7 @@ const { onlyShowFilteredOut } = defineProps<{
 </script>
 
 <template>
-	<Panel :header="title" toggleable :collapsed="true">
+	<Panel :header="title" toggleable :collapsed="true" class="SelectedRecipesInfoBar">
     <ul>
       <li v-for="selectedRecipe in selectedRecipes" :key="selectedRecipe.uid">
         <RecipeCard :recipe="selectedRecipe" type="title" selection-method="icon">
@@ -27,27 +27,39 @@ const { onlyShowFilteredOut } = defineProps<{
         {{totalStillLoadingSelectedRecipes}} recipe{{totalStillLoadingSelectedRecipes !== 1 ? 's' : ''}} still loading
       </li>
     </ul>
+    <Message v-if="!onlyShowFilteredOut" severity="secondary">
+      By the way - you can view your list of selected recipes in Paprika directly. Open and sync Paprika, then in the left-hand sidebar select "Menus" and open  "Spoon Fed - Selected Recipes".
+    </Message>
   </Panel>
 </template>
 
 <style scoped>
-ul {
-  list-style-type: none;
-  padding: 0;
+.SelectedRecipesInfoBar {
+  ul {
+    list-style-type: none;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin: 0.2rem;
+  
+    li {
+      .RecipeCard {
+        width: 100%;
+      }
+      &.status {
+        text-align: center;
+        margin: 0.5rem;
+        font-size: 1.2rem;
+      }
+    }
+  }
+}
+
+/* We don't use nesting here since nesting the deep selector is currently broken https://github.com/vuejs/core/issues/13159 */
+.SelectedRecipesInfoBar :deep(.p-panel-content) {
   display: flex;
   flex-direction: column;
   gap: 1rem;
-  margin: 0.2rem;
-
-  li {
-    .RecipeCard {
-      width: 100%;
-    }
-    &.status {
-      text-align: center;
-      margin: 0.5rem;
-      font-size: 1.2rem;
-    }
-  }
 }
 </style>
